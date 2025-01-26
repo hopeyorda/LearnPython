d5b102139f954068b2334043092c83b372ffda0d
hopeyorda <hopeyorda@gmail>
2025-01-26 16:51:40 +0300

    Fourth Commit

---
+++ while_loop.py
@@ -0,0 +1,11 @@
+#name = input("Enter Your name : ")
+
+#while name == "":
+   # print("You did't enter your name")
+    #name = input("Enter Your name : ")
+
+#print(f"Welcome {name}")
+num = 0
+while num <10 :
+    print(num)
+    num = num +1
